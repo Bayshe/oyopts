9092983350306ab6b4bf513208c7ad912039791e
Basit Olayinka <olayinkabasit309@gmail.com>
2025-01-01 16:49:48 +0100

    contact page edit (all screens)

--- headerFooter.js
+++ headerFooter.js
@@ -3,7 +3,7 @@ class SpecialHeader extends HTMLElement {
         this.innerHTML = `
         <header>
             <div class="navbar">
-                <div class="logo"><a href="#">PTS</a></div>
+                <div class="logo"><a href="/index.html">PTS</a></div>
                 <ul class="links">
                     <li><a href="index.html">Home</a></li>
                     <li><a href="about.html">About</a></li>
