4b2bbbcb0340cbb4fad12aaef286682ce1595c96
Basit Olayinka <olayinkabasit309@gmail.com>
2025-02-05 22:37:51 +0100

    removed contact from nav bar

--- headerFooter.js
+++ headerFooter.js
@@ -8,7 +8,7 @@ class SpecialHeader extends HTMLElement {
                     <li><a href="index.html">Home</a></li>
                     <li><a href="about.html">About</a></li>
                     <li><a href="services.html">Services</a></li>
-                    <li><a href="contact.html">Contact</a></li>
+                    
                 </ul>
                 <a href="http://ptsexpress.netlify.app" class="action_btn">Track Order</a>
                 <div class="toggle_btn">
@@ -21,7 +21,7 @@ class SpecialHeader extends HTMLElement {
                     <li><a href="index.html">Home</a></li>
                     <li><a href="about.html">About</a></li>
                     <li><a href="services.html">Services</a></li>
-                    <li><a href="contact.html">Contact</a></li>
+                    
                     <li><a href="http://ptsexpress.netlify.app" class="action_btn">Track Order</a></li>
                 </ul>
             </div>
